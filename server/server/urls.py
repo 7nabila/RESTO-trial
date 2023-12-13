36cd2489767ad5af399354965efafe670f1f4200
StellaJuvGrace <148447394+StellaJuvGrace@users.noreply.github.com>
2023-12-13 17:21:02 +0700

    Add files via upload
    tambahkan server dalam server

---
+++ server/server/urls.py
@@ -0,0 +1,32 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import TemplateView
+from rest_framework.schemas import get_schema_view
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/menus/', include('menus.urls')),
+    path('api/', include('profiles.urls')),
+    path('swagger-ui/', TemplateView.as_view(
+        template_name='swagger-ui.html',
+        extra_context={'schema_url':'openapi-schema'}
+    ), name='swagger-ui'),
+    path('redoc/', TemplateView.as_view(
+        template_name='redoc.html',
+        extra_context={'schema_url':'openapi-schema'}
+    ), name='redoc'),
+    path('openapi', get_schema_view(
+        title="Restaurant API",
+        description="API untuk Website Restaurant Keluarga Solo",
+        version="1.0.0"
+    ), name='openapi-schema'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
